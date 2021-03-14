27984104d6c6c3e3a558c5d6ed4ace2a9093285e
Nathan Lilienthal <nathan@nixpulvis.com>
2021-03-14 15:27:38 -0400

    Invert them since our addresses are actually signed

--- src/bin/galos-sync/eddb.rs
+++ src/bin/galos-sync/eddb.rs
@@ -26,23 +26,28 @@ impl Run for Cli {
             .progress_chars("##-"));
         for result in bar.wrap_iter(dump.into_iter()) {
             if let Ok(system) = result {
-                if let Some(address) = system.ed_system_address {
-                    let position = Coordinate {
-                        x: system.coords.x,
-                        y: system.coords.y,
-                        z: system.coords.z
-                    };
-                    task::block_on(async {
-                        let result = System::create(db, address, &system.name, position,
-                            system.population, system.security, system.government,
-                            system.allegiance, system.primary_economy, None, system.updated_at)
-                            .await;
-                        match result {
-                            Ok(_) => bar.set_message(&format!("[EDDB] {}", system.name)),
-                            Err(err) => bar.set_message(&format!("[EDDB ERROR] {}", err)),
-                        }
-                    });
-                }
+                let address = if let Some(a) = system.ed_system_address {
+                    a as i64
+                } else {
+                    -(system.id as i64)
+                };
+
+                let position = Coordinate {
+                    x: system.coords.x,
+                    y: system.coords.y,
+                    z: system.coords.z
+                };
+
+                task::block_on(async {
+                    let result = System::create(db, address as u64, &system.name, position,
+                        system.population, system.security, system.government,
+                        system.allegiance, system.primary_economy, None, system.updated_at)
+                        .await;
+                    match result {
+                        Ok(_) => bar.set_message(&format!("[EDDB] {}", system.name)),
+                        Err(err) => bar.set_message(&format!("[EDDB ERROR] {}", err)),
+                    }
+                });
             }
         }
     }
